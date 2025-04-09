abc8ae6124ef1f6d53b3fb95800cf2923f15aa8e
Toby Leftly <toby@leftly.me>
2025-04-09 11:45:15 -0400

    base change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://certabooks.ca',
-  base: 'certabooks',
+  // base: 'certabooks',
   vite: {
     plugins: [tailwindcss()]
   }
